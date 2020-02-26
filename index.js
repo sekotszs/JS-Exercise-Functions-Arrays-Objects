0da47d084ab3dc2eaf356ce5aecd3f53edaf28ac
Zoe Stokes <sekotszs@gmail.com>
2020-02-26 06:52:25 -0800

    almost done

--- index.js
+++ index.js
@@ -263,8 +263,18 @@ function getOlderCars(inventory,year) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
+function getGermanCars(inventory) {
   /* code here */
+  const gcars= []
+  for (let i=0; i<inventory.length; i++){
+    if (inventory[i].car_make==='Audi'||
+      inventory[i].car_make=== `Mercedes-Benz`||
+      inventory[i].car_make===`Volkswagen`||
+      inventory[i].car_make===`BMW`){
+      gcars.push (inventory[i])
+    }
+  }
+  return gcars
 }
 
 /**
@@ -285,7 +295,9 @@ function getGermanCars(/* code here */) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
+const sum = (a,b)=>{
+  return a+b
+}
 const addFive = null; // code here!
 const argTimesTwo = null; // code here!
 
